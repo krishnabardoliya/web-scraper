25ae7dccad4e322365a7c709d7ab863fa5453298
Krishna <krishnabardoliya@gmail.com>
2019-11-20 01:18:40 +0530

    added jest test example

--- client/src/__test__/UserForm.spec.js
+++ client/src/__test__/UserForm.spec.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { render } from 'react-dom'
+
+jest.mock('react-dom')
+// jest.mock('../src/store/index', () => () => ({
+//     subscribe: jest.fn(),
+//     dispatch: jest.fn(),
+//     getState: jest.fn()
+// }))
+
+import Form from '../component/form.component';
 
 configure({adapter: new Adapter()});
 
-import Form from '../component/form.component';
+describe("App Entry Point - /src/index.js", () => {
+
+    it("renders app wihtout error", () => {
+        require('../index.js')
+    })
+
+})
+
+
+
 
 describe('<Form />', () => {
 	it('Testing on React components, renders without crashing', () => {
